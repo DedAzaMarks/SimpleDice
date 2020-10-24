455ec683a09cca4561a5aee2a32c195f9d392c6d
DedAzaMarks <zibumzibumich@gmail.com>
2020-10-24 14:27:18 +0300

    Release v1.0

---
+++ Assets/Resourses/Scripts/RollADice.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RollADice : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject dice;
+	Rigidbody rb;
+
+    private void Start()
+    {
+		rb = dice.GetComponent<Rigidbody>();
+    }
+
+    public void Click()
+    {
+		ResetScript.Reset();
+		float dirX = Random.Range(-500, 500);
+		float dirY = Random.Range(-500, 500);
+		float dirZ = Random.Range(-500, 500);
+		dice.transform.position = new Vector3(0, 5, 0);
+		dice.transform.rotation = Quaternion.identity;
+		rb.AddForce(new Vector3(dirX, dirY, dirZ));
+		rb.AddTorque(dirX, dirY, dirZ);
+	}
+}
